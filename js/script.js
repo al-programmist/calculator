ba3a5d134c6348d63a5cbfc58b62a33159d65530
Alexander Guzyaev <5229013-al-programmist@users.noreply.gitlab.com>
2023-12-01 21:35:21 +0300

    Прописали кнопки отключения, очистки и +

--- js/script.js
+++ js/script.js
@@ -5,16 +5,26 @@
  var output = null;
  var keyboard = null;
  var off = null;
+ var reset = null;
+ var plus = null;
+ var minus = null;
+ var divide = null;
+ var multiply = null;
+ var sqrt = null;
+ var equal = null;
  var buffer = 0;
+ var comparator = [];
 
  function selectFromVirtualKeyboard(value) {
    if (Number(value) >= 0 || value === '.') {
-    var virtualKeyboardSelector = document.querySelector('[data-number="' + value + '"]');
-     virtualKeyboardSelector.classList.add('on-keyboard--selected');
+     var virtualKeyboardSelector = document.querySelector('[data-number="' + value + '"]');
 
-     setTimeout(function () {
-        virtualKeyboardSelector.classList.remove('on-keyboard--selected');
-     }, 300);
+     if (virtualKeyboardSelector) {
+        virtualKeyboardSelector.classList.add('on-keyboard--selected');
+         setTimeout(function () {
+           virtualKeyboardSelector.classList.remove('on-keyboard--selected');
+         }, 300);
+     }
    }
  }
 
@@ -27,6 +37,30 @@
    }
  }
 
+  function setControlValue(symbol) {
+    var currentSymbol = String(symbol).trim().toLowerCase();
+    var virtualControlSelector = document.querySelector('[data-action="' + currentSymbol + '"]');
+
+    if (virtualControlSelector) {
+      virtualControlSelector.classList.add('on-keyboard--selected');
+
+      setTimeout(function () {
+        virtualControlSelector.classList.remove('on-keyboard--selected');
+      }, 300);
+    }
+
+    if (currentSymbol === 'delete') {
+      output.value = '0';
+      buffer = 0;
+    }
+
+    if (currentSymbol === 'backspace') {
+      output.value = '0';
+      buffer = 0;
+    }
+
+  }
+
  function setKeyboardOutput(event) {
    // Разрешаем: backspace, delete, tab и escape
    if ( event.keyCode === 46 || event.keyCode === 8 || event.keyCode === 9 || event.keyCode === 27 ||
@@ -37,6 +71,7 @@
            // Разрешаем десятичную точку
            (event.keyCode === 110)) {
      if(event.keyCode === 110) setKeyboardValue(event.key);
+     if(event.keyCode === 46 || event.keyCode === 8) setControlValue(event.key);
      return;
    } else {
      setKeyboardValue(event.key);
@@ -82,11 +117,49 @@
    }
  }
 
- function triggerOnOff(event) {
-   event.preventDefault();
+ function disableCalculator() {
    output.value = '0';
    buffer = 0;
+   comparator = [];
    output.toggleAttribute('disabled');
+   reset.toggleAttribute('disabled');
+   plus.toggleAttribute('disabled');
+   minus.toggleAttribute('disabled');
+   divide.toggleAttribute('disabled');
+   multiply.toggleAttribute('disabled');
+   sqrt.toggleAttribute('disabled');
+   equal.toggleAttribute('disabled');
+
+   keyboard.forEach(function (button){
+     button.toggleAttribute('disabled');
+   })
+ }
+
+ function triggerOnOff(event) {
+   event.preventDefault();
+   disableCalculator();
+ }
+
+ function setComparator() {
+   var currentValue = Number(buffer);
+   // output.value = '0';
+   comparator.push(currentValue);
+   buffer = 0;
+ }
+
+ function makeSum() {
+   var result = 0;
+   setComparator();
+   comparator.forEach(function (value){
+     result += value;
+   });
+
+  return result;
+ };
+
+ function plusHandler(event) {
+   event.preventDefault();
+   output.value = makeSum();
  }
 
  function init() {
@@ -95,10 +168,23 @@
    output = widget.querySelector('.js-screen-input');
    keyboard = widget.querySelectorAll('.js-widget-number');
    off = widget.querySelector('.js-widget-off');
+   reset = widget.querySelector('.js-widget-reset');
+   plus = widget.querySelector('.js-widget-plus');
+   minus = widget.querySelector('.js-widget-minus');
+   divide = widget.querySelector('.js-widget-divide');
+   multiply = widget.querySelector('.js-widget-multiply');
+   sqrt = widget.querySelector('.js-widget-sqrt');
+   equal = widget.querySelector('.js-widget-equal');
 
 
    widget.addEventListener('reset', resetHandler);
    off.addEventListener('click', triggerOnOff);
+   plus.addEventListener('click', plusHandler);
+   // minus.addEventListener('click', minusHandler);
+   // divide.addEventListener('click', divideHandler);
+   // multiply.addEventListener('click', multiplyHandler);
+   // sqrt.addEventListener('click', sqrtHandler);
+   // equal.addEventListener('click', equalHandler);
 
    keyboard.forEach(function (value){
      value.addEventListener('click', clickKeyboardNumberHandler);
